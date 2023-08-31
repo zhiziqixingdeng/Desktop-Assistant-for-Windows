be23c4b2edd2ce6c0116f4b7244057d538228b54
zhizi质子 <139207819+zhiziqixingdeng@users.noreply.github.com>
2023-08-31 08:44:21 +0800

    Create zhizi-cmd.cpp

---
+++ cpp/zhizi-cmd.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+using namespace std;
+string str;
+int main(){
+	cout<<"欢迎来到zhizi-cmd"<<endl;
+	sleep(1);
+	system("cls");
+	while(str!="end"){
+		cin>>str;
+		if(str=="cls"){
+			system("cls");
+		}
+		else if(str==""){
+			cout<<"输入不能为空"<<endl;
+		}
+		else if(str=="student"){
+			system("start student");
+		}
+		else if(str=="end"){
+			cout<<"再见"<<ENDL;
+		}
+		else{
+			cout<<"错误"<<endl;
+		}
+	}
+	return 0;
+}
